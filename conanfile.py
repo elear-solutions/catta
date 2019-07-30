47ef66df77b83ce465e5719762ac2a7a840f4c99
amiya-elear <amiyasantra@elear.solutions>
2019-07-30 13:04:58 +0530

    change version name in conanfile

--- conanfile.py
+++ conanfile.py
@@ -2,11 +2,11 @@ from conans import ConanFile, AutoToolsBuildEnvironment, tools
 
 class CattalibConan(ConanFile):
     name = "catta"
-    version = "0.1"
+    version = "0.0.1"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Cattalib here>"
+    description = "This recipe file used to build and package binaries of cocostandard repository"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
